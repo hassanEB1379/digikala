30e03074732b59b71c893434a33c4e9106fc09bd
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-21 18:05:59 +0430

    update offers submenu and create submenu for mobile

---
+++ src/components/block/Header/components/Menu/Offers.js
@@ -0,0 +1,81 @@
+import { main } from "-data/categories";
+import Banner from "components/micro/Banner";
+import Fade from "components/micro/Fade";
+import Collapse from "../../../../micro/Collapse";
+
+export const OffersDesk = ({ show }) => {
+   return (
+      <Fade
+         show={show}
+         className="w-md bg-white rounded-b-xl shadow-header flex gap-8 p-4 absolute top-full"
+      >
+         <div className="w-3/5">
+            <p className="mb-8 font-bold">
+               <a href="/" className="hover:text-red-500">
+                  مشاهده همه تخفیف ها و پیشنهاد ها
+               </a>
+            </p>
+
+            <ul className="flex flex-col flex-wrap gap-4 max-h-96">
+               <li className="w-1/2 font-bold text-lg">
+                  <a href="/" className="hover:text-red-500">
+                     کالا های شگفت انگیز
+                  </a>
+               </li>
+
+               <li className="w-1/2 font-bold text-lg">
+                  <a href="/" className="hover:text-red-500">
+                     شگفت انگیز سوپر مارکتی
+                  </a>
+               </li>
+
+               <li className="w-1/2 font-bold text-lg">
+                  <a href="/" className="hover:text-red-500">
+                     فروش ویژه
+                  </a>
+               </li>
+
+               {main.map((category, i) => (
+                  <li key={i} className="w-1/2">
+                     <a href="/" className="hover:text-red-500">
+                        {category.name}
+                     </a>
+                  </li>
+               ))}
+
+               <hr />
+            </ul>
+         </div>
+
+         <div className="w-2/5">
+            <Banner src="/static/images/offers.jpg" />
+         </div>
+      </Fade>
+   );
+};
+
+export const OffersMobile = ({ show }) => {
+   return (
+      <Collapse show={show}>
+         <ul className="pr-14 py-3 space-y-4">
+            <li className="font-bold text-lg">
+               <a href="/" className="hover:text-red-500">
+                  کالا های شگفت انگیز
+               </a>
+            </li>
+
+            <li className="font-bold text-lg">
+               <a href="/" className="hover:text-red-500">
+                  شگفت انگیز سوپر مارکتی
+               </a>
+            </li>
+
+            <li className="font-bold text-lg">
+               <a href="/" className="hover:text-red-500">
+                  فروش ویژه
+               </a>
+            </li>
+         </ul>
+      </Collapse>
+   );
+};
