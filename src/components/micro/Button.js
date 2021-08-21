383c2cb439982f3667c5973fc115e9974b43e1cd
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-21 17:51:58 +0430

    update button component

--- src/components/micro/Button.js
+++ src/components/micro/Button.js
@@ -1,22 +1,29 @@
 import clsx from "clsx";
 
-const Button = ({ children, icon, color = "black", variant = "outlined" }) => {
-  const classes = clsx(
-    "flex gap-4 items-center justify-center cursor-pointer rounded-xl text-center font-medium p-4",
-    {
-      [`border border-${color} text-${color}`]: variant === "outlined",
-    },
-    {
-      [`bg-${color}-500 text-white`]: variant === "contained",
-    }
-  );
+const Button = ({
+   children,
+   iconEnd,
+   iconStart,
+   color = "black",
+   variant = "outlined",
+   classes = {},
+}) => {
+   const ContainerClasses = clsx(
+      "flex gap-4 items-center justify-center cursor-pointer rounded-xl text-center font-medium p-4",
+      {
+         [`border border-${color} text-${color}`]: variant === "outlined",
+         [`bg-${color}-500 text-white`]: variant === "contained",
+         [classes.root]: classes.root,
+      }
+   );
 
-  return (
-    <div className={classes}>
-      {icon}
-      {children}
-    </div>
-  );
+   return (
+      <div className={ContainerClasses}>
+         {iconStart && iconStart}
+         <span className={classes.text}>{children}</span>
+         {iconEnd && iconEnd}
+      </div>
+   );
 };
 
 export default Button;
