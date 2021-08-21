4bb30da4c9b8172a7077a51ee8416461f1aceeb5
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-21 17:55:39 +0430

    update iconBox component

--- src/components/micro/IconBox.js
+++ src/components/micro/IconBox.js
@@ -1,16 +1,18 @@
-const IconBox = ({ iconPath, text }) => {
-  return (
-    <div className="flex flex-col gap-6 items-center">
-      <div className="w-12 h-12">
-        <img
-          className="w-full h-full object-contain"
-          alt="feature1"
-          src={iconPath}
-        />
+const IconBox = ({ iconPath, text, className }) => {
+   return (
+      <div
+         className={["flex flex-col gap-6 items-center", className].join(" ")}
+      >
+         <div className="w-14 h-14">
+            <img
+               className="w-full h-full object-contain"
+               alt="feature1"
+               src={iconPath}
+            />
+         </div>
+         <span>{text}</span>
       </div>
-      <span>{text}</span>
-    </div>
-  );
+   );
 };
 
 export default IconBox;
