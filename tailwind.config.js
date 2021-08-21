55c676e6902b50822ac379cee8e1f7381bbf4ec4
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-21 18:00:08 +0430

    add custom utilities to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,42 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        darkBlue: {
-          DEFAULT: "#3c4b6d",
-        },
+   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+      extend: {
+         colors: {
+            darkBlue: {
+               DEFAULT: "#3c4b6d",
+            },
+         },
+         transitionProperty: {
+            height: "height , max-height",
+            width: "width",
+         },
+         boxShadow: {
+            header: "0 7px 8px 0 rgb(0 0 0 / 4%)",
+         },
+         maxHeight: {
+            sm: "420px",
+         },
+         minHeight: {
+            sm : "350px"
+         },
+         maxWidth: {
+            sm: "350px",
+         },
+         width: {
+            sm: "420px",
+            md: "830px",
+         },
       },
-      transitionProperty: {
-        height: "height",
+      fontFamily: {
+         sans: ["Vazir"],
       },
-      boxShadow: {
-        header: "0 7px 8px 0 rgb(0 0 0 / 4%)",
+   },
+   variants: {
+      extend: {
+         padding: ["first"],
       },
-      maxHeight: {
-        sm: "420px",
-      },
-      width: {
-        sm: "420px",
-        md: "830px",
-      },
-    },
-    fontFamily: {
-      sans: ["Vazir"],
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
